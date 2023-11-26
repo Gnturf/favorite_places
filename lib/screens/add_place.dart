f5b1a02ed29f9bd2db49135a4e3a6dbd92184c9f
Guntur Firmansyah <gfirmansyah279@gmail.com>
2023-11-26 14:02:18 +0700

    First init

---
+++ lib/screens/add_place.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  const AddPlaceScreen({superkey});
+
+  @override
+  State<AddPlaceScreen> createState() {
+    return _AddPlaceScreenState();
+  }
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return 
+  }
+}
