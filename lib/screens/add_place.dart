104b8c79c38d5b30ef597c9375b64bfcb4dc35f0
Guntur Firmansyah <gfirmansyah279@gmail.com>
2023-11-26 15:55:34 +0700

    adding riverpod

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -10,8 +10,38 @@ class AddPlaceScreen extends StatefulWidget {
 }
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return 
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add New Places'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Title',
+              ),
+              controller: _titleController,
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+              label: Text('Add Place'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
